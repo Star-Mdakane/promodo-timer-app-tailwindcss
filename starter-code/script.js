d69aede70dcb70bf4d22edbd7c3adc7fa4e18f15
Star <mmdakane@rocketmail.com>
2026-03-04 22:10:02 +0200

    timer...

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,9 +1,9 @@
 const circle = document.querySelector(".progress-circle")
-const circumference = 2 * Math.PI * 40;
+const circumference = 2 * Math.PI * 105;
 
 function setProgress(percent) {
     const offset = circumference - (percent / 100) * circumference;
-    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.style.strokeDasharray = `${circumference}`;
     circle.style.strokeDashoffset = offset;
 }
 
