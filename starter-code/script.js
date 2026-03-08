4182a54f66403dec293c4b27fceae78f8d7d48a1
Star <mmdakane@rocketmail.com>
2026-03-08 12:46:37 +0200

    buttons select

--- starter-code/script.js
+++ starter-code/script.js
@@ -7,3 +7,40 @@ function setProgress(percent) {
 }
 
 setProgress(60)
+
+const settings = document.getElementById("settings");
+const settingsClose = document.getElementById("close-settings");
+const timerSelect = document.querySelectorAll("#timer-btns button");
+const fonts = document.querySelectorAll("#fonts button");
+const colors = document.querySelectorAll("#colors button");
+
+settings.addEventListener("click", (e) => {
+    const settingsGroup = e.target.closest(".group\\/settings");
+    settingsGroup.classList.add("isOpen");
+});
+
+settingsClose.addEventListener("click", (e) => {
+    const settingsGroup = e.target.closest(".group\\/settings");
+    settingsGroup.classList.remove("isOpen");
+});
+
+timerSelect.forEach(select => {
+    select.addEventListener("click", () => {
+        timerSelect.forEach(btn => btn.classList.remove("isActive"));
+        select.classList.add("isActive");
+    })
+})
+
+fonts.forEach(select => {
+    select.addEventListener("click", () => {
+        fonts.forEach(btn => btn.classList.remove("isActive"));
+        select.classList.add("isActive");
+    })
+})
+
+colors.forEach(select => {
+    select.addEventListener("click", () => {
+        colors.forEach(btn => btn.classList.remove("isActive"));
+        select.classList.add("isActive");
+    })
+})
